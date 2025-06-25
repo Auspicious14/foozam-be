0792e8016451108cc334f0011728cfa028cc18f9
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-06-25 12:37:21 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -3,18 +3,13 @@ import mongoose from "mongoose";
 import cors from "cors";
 import { json } from "body-parser";
 import dotenv from "dotenv";
-import foodRoutes from "./routes";
+import {appRoute} from "./index"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
-app.use(json());
-
-// MongoDB Connection
 mongoose
   .connect(process.env.MONGODB_URL || "", {
     useNewUrlParser: true,
@@ -23,18 +18,12 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// API Routes
-app.use("/api/foods", foodRoutes);
+app.use(appRoute);
 
-// Health check
-app.get("/api/health", (_, res) => res.json({ status: "ok" }));
-
-// Start server (for local dev)
 if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
 }
 
-// For Vercel serverless
 module.exports = app;
