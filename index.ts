f5b8a71403e057b0f6e67e3e4ee38f881bb474b1
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-06-25 12:32:18 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,10 +2,12 @@ import dotenv from "dotenv";
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-export const appRoute = express();
 import { errorHandler } from "./middlewares/error";
+import foodRoutes from "./routes"
 
 dotenv.config();
+
+export const appRoute = express();
 
 const allowedOrigins = process.env.CLIENT_URL
   ? process.env.CLIENT_URL.split(",").map((url) => url.trim())
@@ -38,6 +40,12 @@ console.log("ENV:", process.env.CLIENT_URL);
 appRoute.use(express.json({ limit: "50mb" }));
 appRoute.use(express.urlencoded({ limit: "50mb", extended: true }));
 appRoute.use(cookieParser());
+
 appRoute.get("/", (req, res) => {
   res.send("Backend is working!");
 });
+
+// Health check
+appRoute.get("/api/health", (_, res) => res.json({ status: "ok" }));
+
+appRoute.use(foodRoutes)
